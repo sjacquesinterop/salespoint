fc0e85b6e273a7d8662e1a6a7e4dc7565e96fce9
Félix Larouche <flarouche3129@gmail.com>
2021-02-25 14:24:30 -0500

    Initialized the project
    Initialized the project

---
+++ src/main/java/com/felix/projects/salespoint/SalespointApplication.java
@@ -0,0 +1,13 @@
+package com.felix.projects.salespoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalespointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalespointApplication.class, args);
+	}
+
+}
